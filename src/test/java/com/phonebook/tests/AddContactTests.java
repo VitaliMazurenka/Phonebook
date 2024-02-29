357e9ed08324f44b8ec8e66dcf1df3327b3fdd4d
Vitali Mazurenka <vitali.mazurenka@web.de>
2024-02-29 22:15:06 +0100

    add main, helpers, new task in build.gradle,ApplicationManager

--- src/test/java/com/phonebook/tests/AddContactTests.java
+++ src/test/java/com/phonebook/tests/AddContactTests.java
@@ -1,43 +1,41 @@
 package com.phonebook.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import com.phonebook.models.Contact;
+import com.phonebook.models.User;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
 public class AddContactTests extends TestBase {
     @BeforeMethod
     public void precondition() {
-        if (!isLoginLinkPresent()) {
-            clickOnSignOutButton();
+        if (!app.getUser().isLoginLinkPresent()) {
+            app.getUser().clickOnSignOutButton();
         }
-        clickOnLoginLink();
-        fillLoginRegisterForm(new User()
+        app.getUser().clickOnLoginLink();
+        app.getUser().fillLoginRegisterForm(new User()
                 .setEmail("kotopes@mail.com")
                 .setPassword("Kot124_$!"));
-        clickOnLoginButton();
+        app.getUser().clickOnLoginButton();
     }
     @Test
     public void addContactPositiveTest() {
-        clickOnAddLink();
-        fillContactForm(new Contact()
+        app.getContact().clickOnAddLink();
+        app.getContact().fillContactForm(new Contact()
                 .setName("Adam")
                 .setLastName("Nickson")
                 .setPhone("9998277727")
                 .setEmail("adam1@gmail.ru")
                 .setAddress("Berlin")
                 .setDescription("goalkeeper"));
-        clickOnSaveButton();
-        Assert.assertTrue(isContactCreatedByText("Adam"));
+        app.getContact().clickOnSaveButton();
+        Assert.assertTrue(app.getContact().isContactCreatedByText("Adam"));
     }
 
     @AfterMethod
     public void postCondition() {
-        removeContact();
+        app.getContact().removeContact();
     }
 
 }
--- src/test/java/com/phonebook/tests/LoginTests.java
+++ src/test/java/com/phonebook/tests/LoginTests.java
@@ -1,6 +1,6 @@
 package com.phonebook.tests;
 
-import org.openqa.selenium.By;
+import com.phonebook.models.User;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -8,29 +8,28 @@ import org.testng.annotations.Test;
 public class LoginTests extends TestBase {
     @BeforeMethod
     public void ensurePrecondition() {
-        if (!isLoginLinkPresent()) {
-            clickOnSignOutButton();
+        if (!app.getUser().isLoginLinkPresent()) {
+            app.getUser().clickOnSignOutButton();
         }
     }
 
     @Test
     public void loginPositiveTest() {
-        clickOnLoginLink();
-        fillLoginRegisterForm(new User()
+        app.getUser().clickOnLoginLink();
+        app.getUser().fillLoginRegisterForm(new User()
                 .setEmail("kotopes@mail.com")
                 .setPassword("Kot124_$!"));
-        clickOnLoginButton();
+        app.getUser().clickOnLoginButton();
         //assert Sign Out is present
-        Assert.assertTrue(isSignOutButtonPresent());
+        Assert.assertTrue(app.getUser().isSignOutButtonPresent());
     }
     @Test
     public void loginNegativeTestWithoutEmail() {
-        clickOnLoginLink();
-        fillLoginRegisterForm(new User()
+        app.getUser().clickOnLoginLink();
+        app.getUser().fillLoginRegisterForm(new User()
                 .setPassword("Kot124_$!"));
-        clickOnLoginButton();
-        //assert Sign Out is present
-        Assert.assertTrue(isAlertAppears());
+        app.getUser().clickOnLoginButton();
+        Assert.assertTrue(app.getUser().isAlertAppears());
     }
 
 }
